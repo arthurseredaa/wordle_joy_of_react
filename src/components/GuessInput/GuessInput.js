b51342db2b019400e992a078d09395b498ce7953
arthursereda <arthur.sereda@promova.com>
2024-05-16 22:52:26 +0300

    complete exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function GuessInput() {
+  const [guessValue, setGuessValue] = useState('');
+
+  const handleChange = (e) => setGuessValue(e.target.value.toUpperCase());
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setGuessValue('');
+
+    console.log({ guess: guessValue });
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guessValue}
+        onChange={handleChange}
+        pattern="\w{5,5}"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
